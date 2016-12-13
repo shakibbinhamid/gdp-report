00057b75a34a2d77c55f713ef29a0e208476b217
Shakib-Bin Hamid <sh3g12@soton.ac.uk>
2016-12-13 11:31:07 +0000

    Put back the makefiles after pushing to overleaf

---
+++ make.bat
@@ -0,0 +1,14 @@
+pdflatex final.tex
+bibtex final.aux
+pdflatex final.tex
+pdflatex final.tex
+
+del *.aux
+del *.log
+del *.bbl
+del *.blg
+del *.lot
+del *.lof
+del *.toc
+del *.out
+del chapters/*.log
